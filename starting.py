43675d28627824ced84e1e66a1f40dcef6f9a5c4
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2020-05-12 20:22:10 +0530

    made classes

--- starting.py
+++ starting.py
@@ -1,193 +1,154 @@
 import pygame
-from pygame.locals import *
-import astar
-display_width = 1020+2+200
-display_height = 810+2-30*5
+from astar import Astar
+import time
+class Game(Astar):
 
-pygame.init()
-screen = pygame.display.set_mode((display_width,display_height))
-clock = pygame.time.Clock()
-pygame.display.set_caption(u'Algorithm Visualization')
+	def __init__(self):
+		Astar.__init__(self)
+		pygame.init()
+		self.display_width = 1020+2+200
+		self.display_height = 570+2
+		self.screen = pygame.display.set_mode((self.display_width,self.display_height))
+		self.clock = pygame.time.Clock()
+		pygame.display.set_caption(u'Algorithm Visualization')
+		self.width = 34
+		self.height =22
 
-white = (255,255,255)
-red = (255,0,0)
-blue = (0,255,0)
-green = (0,0,255)
-grid  = (0,150,255)
+		self.walls = list()
+		self.maze = [[]]
 
-black = (0,0,0)
+		self.white = (255,255,255)
+		self.red = (255,0,0)
+		self.less_red = (150,0,0)
+		self.blue = (0,255,0)
+		self.less_blue = (0,150,0)
+		self.green = (0,0,255)
+		self.less_green = (0,0,150)
+		self.black = (0,0,0)
+		self.grid_color = (0,150,255)
 
-path = []
-walls = []
-start_node = ()
-end_node = ()
+	def text_objects(self,message,font):
+		textSurface = font.render(message,False,self.white)
+		return textSurface,textSurface.get_rect()
 
-		
+	def button(self,msg,x,y,w,h,ic,ac,action=None):
+		mouse = pygame.mouse.get_pos()
+		click = pygame.mouse.get_pressed()
 
+		pygame.draw.rect(self.screen,ic,(x,y,w,h))
+		if x+w > mouse[0] > x and y+h > mouse[1] > y:
+				pygame.draw.rect(self.screen,ac,(x,y,w,h))
+				if click[0] == 1 and action != None:
+					time.sleep(0.5)
+					action()
 
-def button(msg,x,y,w,h,ic,ac,action = None):
-	mouse = pygame.mouse.get_pos()
-	click = pygame.mouse.get_pressed()
-
-	pygame.draw.rect(screen,ac,(x,y,w,h))
-	if x+w > mouse[0] > x and y+h > mouse[1] > y:
-		pygame.draw.rect(screen,ic,(x,y,w,h))
-		if click[0] == 1 and action != None:
-
-
-			action()
-
-
-	font = pygame.font.SysFont("freesansbold.ttf",25)
-	textSurface ,textRect = text_objects(msg,font)
-	textRect.center = ( (x+(w/2)), (y+(h/2)) )
-	screen.blit(textSurface, textRect)
-
-
-def welcome():
-
-	gameExit = False
-
-	while not gameExit:
-		for events in pygame.event.get():
-			if events.type == pygame.QUIT:
-				pygame.quit()
-	
-		message = ['Welcome to this game, Shashank is lonely as fuck','all he know are different pathfinding algorithms,','he will use different algorihms to find his girlfriend']
-		counter = 0
-		for message in message:
-			text = pygame.font.Font('freesansbold.ttf',30)
-
-			TextSurf, TextRect = text_objects(message,text)
+		font = pygame.font.SysFont("freesansbold.ttf",25)
+		textSurface ,textRect = self.text_objects(msg,font)
+		textRect.center = ( (x+(w/2)), (y+(h/2)) )
+		self.screen.blit(textSurface, textRect)
 
 
-			TextRect.center = ((display_width/2),(display_height/2-150+counter))
+	def welcome_screen(self):
 
-			counter+=30
-			screen.blit(TextSurf, TextRect)
+		game_exit = False
 
-		button("G O !",500,450,100,50,green,green,start_game)
+		while not game_exit:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					game_exit = True
+					pygame.quit()
 
-		pygame.display.update()
-		screen.fill((0,0,0))
+
+			message = ['Welcome to this game, Samarth is lonely as fuck','all he know are different pathfinding algorithms,','he will use different algorihms to find his girlfriend']
+			width_counter = 0
+			for message in message:
+				text = pygame.font.Font('freesansbold.ttf',30)
+				TextSurf, TextRect = self.text_objects(message,text)
+				TextRect.center = ((self.display_width/2),(self.display_height/2-150+width_counter))
+				width_counter+=30
+				self.screen.blit(TextSurf, TextRect)
+
+				self.button("G O !",500,450,100,50,self.red,self.less_red,self.construct_maze)
+
+				pygame.display.update()
+				self.clock.tick(60)
+
+	def construct_maze(self):
+		game_exit = False
+		draw_grid = False
+		self.screen.fill(self.black)
+		while not game_exit:
+			self.button("Start Node='s'",1025,50,190,50,self.red,(0,0,50),action = None)
+			self.button("end Node='e'",1025,125,190,50,self.red,(0,0,50),action = None)
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					gameExit = True
+
+				elif event.type == pygame.KEYDOWN:
+					x,y = pygame.mouse.get_pos()
+					x = x//30
+					y = y//30
+					if event.key == pygame.K_s:
+						pygame.draw.rect(self.screen,self.red,(x*30,y*30,29,29))
+						self.start_node = (x,y)
+					if event.key == pygame.K_e:
+						pygame.draw.rect(self.screen,self.green,(x*30,y*30,29,29))
+						self.end_node = (x,y)
+
+			if not draw_grid:
+				for x in range(0,1020+30,30):
+					pygame.draw.line(self.screen,self.grid_color,(0,x),(1020,x),2) #horizontal line
+
+					pygame.draw.line(self.screen,self.grid_color,(x,0),(x,1020), 2)
+					self.clock.tick(60)
+					pygame.display.update()
+					draw_grid = True
+
+			
 
 
-def text_objects(message,font):
-	textSurface = font.render(message,False,white)
-	return textSurface,textSurface.get_rect()
-
-def draw_walls():
-	global walls
-	global start_node
-	global end_node
-	
-	button("Start Node='w'",1025,50,190,50,red,(0,0,50),action = None)
-	button("end Node='e'",1025,125,190,50,red,(0,0,50),action = None)
-	while True:
-
-		for events in pygame.event.get():
 			x,y = pygame.mouse.get_pos()
 			x = x//30
 			y = y//30
-			if events.type == pygame.QUIT:
-				pygame.quit()
-			if pygame.mouse.get_pressed()[0] and x*30<1020 and y*30<400+8*30 :
-				
-				
+			if pygame.mouse.get_pressed()[0] and x*30<1020 and y*30<400+8*30:
 
-				pygame.draw.rect(screen,grid,(x*30,y*30,29,29))
+				pygame.draw.rect(self.screen,self.grid_color,(x*30,y*30,29,29))
+				if (x,y) not in self.walls:
+					self.walls.append((x,y))
+
+			self.maze = [[0 for i in range(self.height)]for j in range(self.width)]
+			for x in self.walls:
+				self.maze[x[0]][x[1]] = 1
+
+			self.button("Astar",1025,250,190,50,self.less_blue,(0,0,50),action = self.animate_astar)
+			self.button("BFS",1025,350,190,50,self.less_blue,(0,0,50),action = None)
+
+			pygame.display.update()
+
+	def animate_astar(self):
+		a,path =Astar.search(self,self.maze,1,self.start_node,self.end_node)
+		for a in a:
 			
-				if (x, y) not in walls:
-					walls.append((x,y))
-				
-			if events.type == pygame.KEYDOWN:
-				if events.key == pygame.K_s:
-					
-					start_node = x,y
-					pygame.draw.rect(screen,red,(x*30,y*30,29,29))
-					
-			if events.type == pygame.KEYDOWN:
-				if events.key == pygame.K_e:
-					
+			pygame.draw.rect(self.screen,self.less_red,(a.position[0]*30,a.position[1]*30,29,29))
+			pygame.display.update()
+			self.clock.tick(60)
+		draw = []		
+		for i,j in enumerate(path):
+			for k,l in enumerate(j):
+				if l is not -1:
+					draw.append((i,k))
 
-					end_node = x,y
-					pygame.draw.rect(screen,white,(x*30,y*30,29,29))
-		button("A star",1025,200,190,50,red,(0,0,90),action = construct_maze)
-		pygame.display.update()
-		
-	
-
-def start_game():
-	gameExit = False
-	screen.fill(black)
-	draw_grid = False
-	global walls
-	while not gameExit:
-
-		for events in pygame.event.get():
-			if events.type == pygame.QUIT:
-				pygame.quit()
-				gameExit = True
-
-		if not draw_grid:
-
-			for x in range(0,1020+30,30):
-				pygame.draw.line(screen,grid,(0,x),(1020,x),2) #horizontal line
-
-				pygame.draw.line(screen,grid,(x,0),(x,1020), 2)
-				clock.tick(60)
-				pygame.display.update()
-				draw_grid = True
-
-		button("click here to draw wall",1025,50,190,50,red,(0,0,50),action = draw_walls)
-		pygame.display.update()
-
-
-def construct_maze():
-	
-	global walls
-	global start_node
-	global end_node
-	width = 34
-	global path
-	height = 22
-	maze = [[0 for i in range(height)]for j in range(width)]
-	for x in walls:
-		maze[x[0]][x[1]] = 1
-	draw = []
-	path = astar.search(maze,1,start_node, end_node)
-	for i,j in enumerate(path):
-		for k,l in enumerate(j):
-			if l is not -1:
-				draw.append((i,k))
-
-	for i in draw:
-		print(i)
-		pygame.draw.rect(screen,red,(i[0]*30,i[1]*30,29,29))
-
-
-				
-				
-		pygame.display.update()
-
-	
-# 	draw(path)
-
-
-# def draw(path):
-# 	print(path)
-# 	for i in path:
-# 		print(i)
-# 		pygame.draw.rect(screen,(i[0],i[1],255),(i[0]*30,i[1]*30,29,29))
-# 	pygame.display.update()
+		for i in draw[::-1]:
+			print(i)
+			pygame.draw.rect(self.screen,self.red,(i[0]*30,i[1]*30,29,29))
+			self.clock.tick(60)
+			pygame.display.update()
 
 
 
-def main():
-	welcome()
-	start_game()
 
-main()
+a = Game()
 
-
-
+a.welcome_screen()
